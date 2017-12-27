cf45207c081668bcaaebe6e3cfbab4d379f377b6
Isaac Hier <isaachier@gmail.com>
2017-12-27 09:35:30 -0500

    Rewrite config header every time

--- cmake/AutoutilsCheckDecl.cmake
+++ cmake/AutoutilsCheckDecl.cmake
@@ -10,15 +10,13 @@ include(AutoutilsWriteToConfigHeader)
 macro(autoutils_check_decl decl)
   string(MAKE_C_IDENTIFIER "${decl}" _var)
   string(TOUPPER "${_var}" _var)
-  if(NOT DEFINED _have_symbol_${_var})
-    check_symbol_exists(${decl} "${ARGN}" _have_symbol_${_var})
-    autoutils_write_to_config_header(
-      "#define HAVE_DECL_${_var} ${HAVE_DECL_${_var}}")
-  endif()
+  check_symbol_exists(${decl} "${ARGN}" _have_symbol_${_var})
 
   if(_have_symbol_${_var})
     set(HAVE_DECL_${_var} 1)
   else()
     set(HAVE_DECL_${_var} 0)
   endif()
+  autoutils_write_to_config_header(
+    "#define HAVE_DECL_${_var} ${HAVE_DECL_${_var}}")
 endmacro()
--- cmake/AutoutilsWriteToConfigHeader.cmake
+++ cmake/AutoutilsWriteToConfigHeader.cmake
@@ -3,10 +3,22 @@ if(__autoutils_write_to_config_header)
 endif()
 set(__autoutils_write_to_config_header 1)
 
+set(__autoutils_write_to_config_header_first_invocation ON)
+
 macro(autoutils_write_to_config_header line)
-  if(NOT DEFINED autoutils_config_path)
-    set(autoutils_config_path "${CMAKE_CURRENT_BINARY_DIR}/config.h"
-        CACHE FILEPATH "Path to generated config header")
+  if(__autoutils_write_to_config_header_first_invocation)
+    if(NOT autoutils_config_path)
+      set(autoutils_config_path "${CMAKE_CURRENT_BINARY_DIR}/config.h"
+          CACHE FILEPATH "Path to generated config header" FORCE)
+    endif()
+
+    if(EXISTS "${autoutils_config_path}")
+      message(STATUS "Removing old config and regenerating")
+      file(REMOVE "${autoutils_config_path}")
+    endif()
+
+    set(__autoutils_write_to_config_header_first_invocation OFF)
   endif()
+
   file(APPEND ${autoutils_config_path} "${line}\n")
 endmacro()
